975fe8859251fc8cf6fb5b0cd13c4be0760f1e87
fatiidris <fatiidris2012@gmail.com>
2025-12-29 22:05:26 +0100

    updated

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,6 +1,7 @@
 import image2 from "../Components/Assets/image2.jpg";
 import image8 from "../Components/Assets/image8.jpg";
 import image11 from "../Components/Assets/image11.jpg";
+import image11 from "../Components/Assets/image12.jpg";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
 function Gallery(){
@@ -9,7 +10,7 @@ function Gallery(){
         <NavBar />
         <div className="gallery">
             <div className="gallery-col">
-            <img src={image2} alt="Students" className="std" />
+            <img src={image12} alt="Students" className="std" />
             <img src={image8} alt="Students" className="std" />
             <img src={image11} alt="Students" className="std" />
             </div>
